af025dcca62cc6505fa6abbb669a5fbde7256568
michele.silva <michele.silva@94601c26-d43c-11de-901b-a378632c6355>
2009-11-18 16:59:08 +0000

    Fixed typo in initialize_face_list function.

--- simplex.c
+++ simplex.c
@@ -7,7 +7,7 @@ int insert_simplex(simplex *t, simplex_list *E){return 0;}
 int insert_list(face *f, face_list *AFLa){return 0;}
 int extract_list(face *f, face_list *AFL){return 0;}
 int update_face(face *f1, face_list *AFLa){return 0;}
-void initialize_face_list(face_list *AFL){*AFL == NULL;}
+void initialize_face_list(face_list *AFL){*AFL = NULL;}
 
 /* geometry */
 int intersect(face *f, plane *alpha){return 0;}
