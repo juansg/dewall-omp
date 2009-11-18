25bf04e6d2ecb61871aede1bcda7466c43c9c0b4
michele.silva <michele.silva@94601c26-d43c-11de-901b-a378632c6355>
2009-11-18 16:24:49 +0000

    Created new dewall module to hold functions closely related to the wall construction.

--- trunk/simplex.h
+++ trunk/simplex.h
@@ -36,32 +36,21 @@ typedef int Axis;
 
 enum Axis {XAxis, YAxis};
 
-/* Functions */
+/* IO */
 
 int read_points(char *filename, point_set *P);
 void print_points (FILE *fp, point_set *P);
 int write_simplex_list(char *filename, simplex_list *s);
 
-/* Functions not implemented yet!!!*/
-
-int pointset_partition(point_set *P, plane *alpha, point_set *P1, point_set *P2);
-
-int make_first_simplex(point_set *P, plane *alpha, simplex *s);
-
+/* Face list handling */
 int insert_simplex(simplex *t, simplex_list *E);
-
-int intersect(face *f, plane *alpha);
-
 int insert_list(face *f, face_list *AFLa);
-
 int extract_list(face *f, face_list *AFL);
-
 int make_simplex(face *f, point_set *P, simplex *s);
-
 int update_face(face *f1, face_list *AFLa);
-
 void initialize_face_list(face_list *AFL);
-
 void initialize_plane(point_set *P, plane* alpha, Axis ax);
 
+/* Geometry  */
+int intersect(face *f, plane *alpha);
 Axis invertAxis(Axis ax);
