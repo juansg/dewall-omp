fe3b054a00d1c882b6c1c62d2f5135c9a5c3e35d
cvortmann <cvortmann@94601c26-d43c-11de-901b-a378632c6355>
2009-11-19 03:37:40 +0000

    Adição das funções distance e circumCircleRadius para cálcula da distância entre dois pontos e para cálculo do raio do circum-círculo que envolve 3 pontos, respectivamente.

--- trunk/simplex.c
+++ trunk/simplex.c
@@ -1,17 +1,45 @@
 #include "simplex.h"
+#include <math.h>
 
 /* Functions not implemented yet!!! */
 
 /* face list handling */
-int insert_simplex(simplex *t, simplex_list *E){return 0;}
-int insert_list(face *f, face_list *AFLa){return 0;}
-int extract_list(face *f, face_list *AFL){return 0;}
-int update_face(face *f1, face_list *AFLa){return 0;}
-void initialize_face_list(face_list *AFL){*AFL = NULL;}
+int insert_simplex(simplex *t, simplex_list *E) {
+	return 0;
+}
+int insert_list(face *f, face_list *AFLa) {
+	return 0;
+}
+int extract_list(face *f, face_list *AFL) {
+	return 0;
+}
+int update_face(face *f1, face_list *AFLa) {
+	return 0;
+}
+void initialize_face_list(face_list *AFL) {
+	*AFL = NULL;
+}
 
 /* geometry */
-int intersect(face *f, plane *alpha){return 0;}
+int intersect(face *f, plane *alpha) {
+	return 0;
+}
 
-Axis invertAxis(Axis ax){
-   return !ax;
+Axis invertAxis(Axis ax) {
+	return !ax;
+}
+
+float distance(point a, point b) {
+	return sqrtf(powf(a.x - b.x, 2) + powf(a.y - b.y, 2));
+}
+
+float circumCircleRadius(point a, point b, point c) {
+	float deltaAB = powf(a.x - b.x, 2) + powf(a.y - b.y, 2);
+	float deltaBC = powf(b.x - c.x, 2) + powf(b.y - c.y, 2);
+	float deltaAC = powf(a.x - c.x, 2) + powf(a.y - c.y, 2);
+
+	float dividend = sqrtf(deltaAB) * sqrtf(deltaBC) * sqrtf(deltaAC);
+	float divisor = 2 * (b.x * a.y - c.x * a.y - a.x * b.y + c.x * b.y + a.x * c.y
+			- b.x * c.y);
+	return abs(dividend/divisor);
 }
