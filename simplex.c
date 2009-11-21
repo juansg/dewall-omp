869b1c11ee08e89d98d4fa2b8347f5879836f2d8
cvortmann <cvortmann@94601c26-d43c-11de-901b-a378632c6355>
2009-11-21 00:39:16 +0000

    Implementação do método que calcula o centro do círculo. (Recebendo um warning no main)

--- simplex.c
+++ simplex.c
@@ -1,6 +1,8 @@
 #include "simplex.h"
 #include <math.h>
 
+#define EPSILON 0.000001
+
 /* Functions not implemented yet!!! */
 
 /* face list handling */
@@ -67,4 +69,42 @@ float circumCircleRadius(point *a, point *b, point *c) {
 	if (dividend == 0 || divisor == 0)
 		return 0;
 	return fabsf(dividend / divisor);
+}
+
+point* circumCircleCentre(point *a, point *b, point*c) {
+	float m1, m2, mx1, mx2, my1, my2;
+	float xc, yc;
+	point *centre = (point *) malloc(sizeof(point));
+
+	// Check for coincident points
+	if (fabsf(a->y - b->y) < EPSILON && fabsf(b->y - c->y) < EPSILON)
+		return;
+
+	if (fabsf(b->y - a->y) < EPSILON) {
+		m2 = -(c->x - b->x) / (c->y - b->y);
+		mx2 = (b->x + c->x) / 2.0;
+		my2 = (b->y + c->y) / 2.0;
+		xc = (b->x + a->x) / 2.0;
+		yc = m2 * (xc - mx2) + my2;
+	} else if (fabsf(c->y - b->y) < EPSILON) {
+		m1 = -(b->x - a->x) / (b->y - a->y);
+		mx1 = (a->x + b->x) / 2.0;
+		my1 = (a->y + b->y) / 2.0;
+		xc = (c->x + b->x) / 2.0;
+		yc = m1 * (xc - mx1) + my1;
+	} else {
+		m1 = -(b->x - a->x) / (b->y - a->y);
+		m2 = -(c->x - b->x) / (c->y - b->y);
+		mx1 = (a->x + b->x) / 2.0;
+		mx2 = (b->x + c->x) / 2.0;
+		my1 = (a->y + b->y) / 2.0;
+		my2 = (b->y + c->y) / 2.0;
+		xc = (m1 * mx1 - m2 * mx2 + my2 - my1) / (m1 - m2);
+		yc = m1 * (xc - mx1) + my1;
+	}
+
+	centre->x = xc;
+	centre->y = yc;
+
+	return centre;
 }
