40ef939e31732c9e6f0f73cbf7fe9c2c505c6294
michele.silva <michele.silva@94601c26-d43c-11de-901b-a378632c6355>
2009-11-21 14:08:01 +0000

    A few fixes to circumcentre functionsand update the tests for geometric functions

--- test/geometry/test_geometry.c
+++ test/geometry/test_geometry.c
@@ -3,6 +3,9 @@
 
 int main(){
   point_set P;
+  point centre;
+  float radius;
+
   if (read_points("test_geometry.p", &P) == -1)
     printf("\nFailed to read input points.\n");
   else
@@ -11,7 +14,18 @@ int main(){
       printf ("\ndistance(p1,p2) = %f\n", distance(&(P.point[0]),&(P.point[1])));
       printf ("\ndistance(p1,p3) = %f\n", distance(&(P.point[0]),&(P.point[2])));
       printf ("\ndistance(p2,p3) = %f\n", distance(&(P.point[1]),&(P.point[2])));
-      printf ("\ncircumCircleRadius(p1,p2,p3) = %f\n", circumCircleRadius(&(P.point[0]),&(P.point[1]),&(P.point[2])));   
+      printf ("\ncircumCircleRadius(p1,p2,p3) = %f\n", 
+              circumCircleRadius(&(P.point[0]),&(P.point[1]),&(P.point[2])));
+
+
+      if (circumCircleCentre(&(P.point[0]),&(P.point[1]),&(P.point[2]),&centre))
+        printf("\ncircumCircleCentre(p1,p2,p3) = (%f,%f)\n", centre.x, centre.y);
+
+      if (circumCircleCentreAndRadius(&(P.point[0]),&(P.point[1]),&(P.point[2]),
+              &centre, &radius))
+        printf("\ncircumCircleCentreAndRadius(p1,p2,p3,center,rad) = (%f,%f), "
+              "radius = %f\n", centre.x, centre.y, radius);
+
     } else 
          printf("\nSorry! I need minimum 3 points to try the geometric operations.\n");
   return 0;
