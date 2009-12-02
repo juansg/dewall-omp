be24957c4720fcca8f93f080059ffa72aa734338
cvortmann <cvortmann@94601c26-d43c-11de-901b-a378632c6355>
2009-12-02 21:43:42 +0000

    Utility to create points samples.

---
+++ sample_creator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		printf("sem argumento de entrada");
+		exit(1);
+	}
+	FILE *fp = fopen("sample.txt", "w");
+	if (!fp)
+		exit(1);
+	int sampleSize;
+	sampleSize = atoi(argv[1]);
+	fprintf(fp, "%d\n\n", sampleSize);
+
+	srand(time(NULL));
+
+	float x, y;
+
+	int i;
+	for (i = 0; i < sampleSize; i++) {
+		x = (float) rand() / (float) RAND_MAX * sampleSize - sampleSize / 2;
+		y = (float) rand() / (float) RAND_MAX * sampleSize - sampleSize / 2;
+		fprintf(fp, "%f %f\n", x, y);
+	}
+
+	fclose(fp);
+	exit(0);
+}
