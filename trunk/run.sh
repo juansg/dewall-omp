01418d10b0616fad0f2d9e584a3788f3a9c1eed6
michele.silva <michele.silva@94601c26-d43c-11de-901b-a378632c6355>
2009-12-04 02:28:23 +0000

    UG is giving strange values for triangulations. Skipping it for now.

--- trunk/run.sh
+++ trunk/run.sh
@@ -1,17 +1,19 @@
 export OMP_SET_DYNAMIC=0
 #export OMP_NUM_THREADS=8
 
-rm -f test_results test_time
+rm -f test_results test_time test_timet
 for i in 10 100 1000 10000
 do
 	for j in {1..10}
 	do
 		./sample_creator $i
 		cp sample.txt sample.$i.$j.i.txt
-		{ time ./dewall sample.txt sample.$i.$j.o.txt; } 2>> test_timet
-		wc -l sample.$i.$j.o.txt >> test_results
-		rm -f sample.$i.$j.i.txt sample.$i.$j.o.txt
+		{ time ./dewall sample.$i.$j.i.txt sample.$i.$j.o.txt; } 2>> test_timet
+		wc -l sample.$i.$j.o.txt >> test_results		
 	done
 done
+
+rm -f sample.$i.$j.i.txt sample.$i.$j.o.txt
+
 grep real test_timet | awk '{print $2}' > test_time
 
